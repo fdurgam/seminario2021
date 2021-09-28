e8f8e06e67b26771c920e40151ad43c07d84f2a6
Fernando <fdurgam@gmail.com>
2021-09-28 15:32:02 -0300

    versión para octubre

--- articulos/config/db.php
+++ articulos/config/db.php
@@ -1,10 +1,6 @@
 <?php
-$host = "ec2-52-3-130-181.compute-1.amazonaws.com";
-$user = "uyvfnnuuhfsrrx";
-$password = "3500d56bc9d1e33ba8945d79060e8c6a9e2b7069b871e2db42ff6ba4ffbbbdc5";
-$dbname = "dd3pq672l8kf2o";
-$port = "5432";
 
+include '../includes/config.php';
 try{
   //Set DSN data source name
     $dsn = "pgsql:host=" . $host . ";port=" . $port .";dbname=" . $dbname . ";user=" . $user . ";password=" . $password . ";";
